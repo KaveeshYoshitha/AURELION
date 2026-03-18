8d02c057f12b62de369ecc0f3b9b458153be2091
Kaveesh Yoshitha <yoshithakaveesh83@gmail.com>
2026-03-19 00:15:43 +0530

    Website Initiated

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500 underline ">
+        Welcome to React with TypeScript!
+      </h1>
+    </>
+  );
+}
+
+export default App;
